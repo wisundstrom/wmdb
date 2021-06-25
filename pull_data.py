1fe25410386fdd29369a4b3b0a9160a97a7f973c
wisundstrom <will.a.sundstrom@gmail.com>
2021-06-25 10:43:58 -0400

    made pull data into working headless python script!

---
+++ pull_data.py
@@ -0,0 +1,62 @@
+from neo4j import GraphDatabase
+import pandas as pd
+import numpy as np
+import string
+
+import requests
+from requests.auth import HTTPBasicAuth
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import *
+
+def click_waiter(xpath):
+
+    def clickable(driver):
+        element = driver.find_element_by_xpath(xpath)
+        try:
+            element.click()
+        except ElementClickInterceptedException:
+            return False
+        return element
+    
+    return clickable
+
+profile = webdriver.FirefoxProfile()
+profile.set_preference("browser.download.folderList", 2)
+profile.set_preference("browser.download.manager.showWhenStarting", False)
+profile.set_preference("browser.download.dir", '/home/will/wmdb/wmdb-repo')
+profile.set_preference("browser.download.useDownloadDir", True)
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv")
+
+
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options = options, firefox_profile=profile)
+driver.implicitly_wait(10)
+
+# browser.download.useDownloadDir 
+driver.get()
+assert("Wisundstrom's movies") in driver.title
+
+driver.find_element_by_id("cookiescript_accept").click()
+
+element = WebDriverWait(driver, 15, .5).until(click_waiter("/html/body/div[2]/div[1]/div[1]/div[1]/a[1]"))
+driver.find_element_by_xpath("/html/body/div[2]/div[1]/div[2]/div[2]/div[2]/a[1]").click()
+time.sleep(.5)
+set_cols = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div/form/div/div[2]/div[1]/h2/button")
+# element2 = WebDriverWait(driver, 15, .5).until(click_waiter("/html/body/div[2]/div[3]/div/div/form/div/div[2]/div[1]/h2/button"))
+set_cols.click()
+cols_preset = driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[2]/div/div/div[1]/div[2]/div[4]/div[1]")
+cols_preset.click()
+driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[1]").click()
+driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div/form/div/div[5]/div/a[1]").click()
+
+download = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[3]/div/div/form/div/div[5]/div/a[3]")))
+download.click()
+time.sleep(2)
+driver.close()
