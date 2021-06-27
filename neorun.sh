fc73ca67e961e6e355b8f540849458228c7b9e4f
Will Sundstrom <will.a.sundstrom@gmail.com>
2021-06-27 10:34:17 -0400

    added docker run script for starting neo4j on arm64

---
+++ neorun.sh
@@ -0,0 +1,9 @@
+docker run \
+    --name  cranky_fermi\
+    -p7474:7474 -p7687:7687 \
+    -d \
+    -v $HOME/neo4j/data:/data \
+    -v $HOME/neo4j/logs:/logs \
+    -v $HOME/neo4j/import:/var/lib/neo4j/import \
+    -v $HOME/neo4j/conf:/var/lib/conf \
+    neo4j/neo4j-arm64-experimental:4.3.1-arm64
