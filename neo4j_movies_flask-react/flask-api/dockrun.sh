e2fecc93ce59ad07581fcc17f144d0c1187e2def
Will Sundstrom <will.a.sundstrom@gmail.com>
2021-07-10 17:36:50 -0400

    FINALLY GOT THE REACT SERVER AT LEAST RUNNING ON ARM.  still working on getting it to talk to flask, I can see the react site from pi os

--- neo4j_movies_flask-react/flask-api/dockrun.sh
+++ neo4j_movies_flask-react/flask-api/dockrun.sh
@@ -1,1 +1,1 @@
-docker run -it -p 5000:5000 --network wmdb --user="$(id -u)" --volume=$HOME/wmdb-project/wmdb/neo4j_movies_flask-react/docker_volume:/storage flask_backend
+docker run -it -p 5000:5000 --network wmdb --user="$(id -u)" --volume=$HOME/wmdb-project/wmdb/neo4j_movies_flask-react/docker_volume:/storage flask_backend:dev
