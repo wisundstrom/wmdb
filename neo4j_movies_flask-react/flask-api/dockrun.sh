cce121febbf28b2115bbc453ff5219e3a8cdc1c4
Will Sundstrom <will.a.sundstrom@gmail.com>
2021-07-27 18:44:19 -0400

    switched flask over to prod gunicorn docker

--- neo4j_movies_flask-react/flask-api/dockrun.sh
+++ neo4j_movies_flask-react/flask-api/dockrun.sh
@@ -1,6 +1,6 @@
 docker run -it \
-	-restart unless-stopped \
+	-p 5000:5000 \
        	--network wmdb \
-       	--volume=/home/pi/wmdb-project/wmdb/neo4j_movies_flask-react/flask-api:/app \
-       	--name stupefied_engelbart \ 
-	flask_backend:prod 
+	--restart unless-stopped \
+	--name stupefied_engelbart_prod \
+	flask_backend:prod
